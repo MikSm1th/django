b5946bce87e9beff2726bbebcec3733cb7e8133e
MikSm1th <thiscuriousquest@gmail.com>
2022-01-05 18:43:32 -0500

    Wed 05 Jan 2022 06:43:32 PM EST

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -8,7 +8,7 @@ from . import views
 app_name = 'polls'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
-    #path('owner', views.owner, name='owner'),
+    path('owner', views.owner, name='owner'),
     #ex: /polls/5/
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     #ex: /polls/5/results/
